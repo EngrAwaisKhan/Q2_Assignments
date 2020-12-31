5e7d3dc9a14b29adfdc57e97735bd24301c3986c
Muhammad Awais Khan <66188894+EngrAwaisKhan@users.noreply.github.com>
2020-12-31 13:06:06 +0500

    created assignment no 2

---
+++ piaic66978_Assignment2.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Numpy_Assignment_2::
+
+# ## Question:1
+
+# ### Convert a 1D array to a 2D array with 2 rows?
+
+# #### Desired output::
+
+# array([[0, 1, 2, 3, 4],
+#         [5, 6, 7, 8, 9]])
+
+# In[ ]:
+
+
+
+
+
+# ## Question:2
+
+# ###  How to stack two arrays vertically?
+
+# #### Desired Output::
+array([[0, 1, 2, 3, 4],
+        [5, 6, 7, 8, 9],
+       [1, 1, 1, 1, 1],
+       [1, 1, 1, 1, 1]])
+# In[ ]:
+
+
+
+
+
+# ## Question:3
+
+# ### How to stack two arrays horizontally?
+
+# #### Desired Output::
+array([[0, 1, 2, 3, 4, 1, 1, 1, 1, 1],
+       [5, 6, 7, 8, 9, 1, 1, 1, 1, 1]])
+# In[ ]:
+
+
+
+
+
+# ## Question:4
+
+# ### How to convert an array of arrays into a flat 1d array?
+
+# #### Desired Output::
+array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+# In[ ]:
+
+
+
+
+
+# ## Question:5
+
+# ### How to Convert higher dimension into one dimension?
+
+# #### Desired Output::
+array([ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
+# In[ ]:
+
+
+
+
+
+# ## Question:6
+
+# ### Convert one dimension to higher dimension?
+
+# #### Desired Output::
+array([[ 0, 1, 2],
+[ 3, 4, 5],
+[ 6, 7, 8],
+[ 9, 10, 11],
+[12, 13, 14]])
+# In[ ]:
+
+
+
+
+
+# ## Question:7
+
+# ### Create 5x5 an array and find the square of an array?
+
+# In[ ]:
+
+
+
+
+
+# ## Question:8
+
+# ### Create 5x6 an array and find the mean?
+
+# In[ ]:
+
+
+
+
+
+# ## Question:9
+
+# ### Find the standard deviation of the previous array in Q8?
+
+# In[ ]:
+
+
+
+
+
+# ## Question:10
+
+# ### Find the median of the previous array in Q8?
+
+# In[ ]:
+
+
+
+
+
+# ## Question:11
+
+# ### Find the transpose of the previous array in Q8?
+
+# In[ ]:
+
+
+
+
+
+# ## Question:12
+
+# ### Create a 4x4 an array and find the sum of diagonal elements?
+
+# In[ ]:
+
+
+
+
+
+# ## Question:13
+
+# ### Find the determinant of the previous array in Q12?
+
+# In[ ]:
+
+
+
+
+
+# ## Question:14
+
+# ### Find the 5th and 95th percentile of an array?
+
+# In[ ]:
+
+
+
+
+
+# ## Question:15
+
+# ### How to find if a given array has any null values?
+
+# In[ ]:
+
+
+
+
