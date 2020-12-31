9e7f22ea09b6bd97d15e1862ad3c1f0575a7b44a
Muhammad Awais Khan <66188894+EngrAwaisKhan@users.noreply.github.com>
2020-12-31 13:05:44 +0500

    created assignment no 1
    of numpy

---
+++ piaic66978_Assignment1.py
@@ -0,0 +1,423 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # **Assignment For Numpy**
+
+# Difficulty Level **Beginner**
+
+# 1. Import the numpy package under the name np
+
+# In[ ]:
+
+
+
+
+
+# 2. Create a null vector of size 10 
+
+# In[ ]:
+
+
+
+
+
+# 3. Create a vector with values ranging from 10 to 49
+
+# In[ ]:
+
+
+
+
+
+# 4. Find the shape of previous array in question 3
+
+# In[ ]:
+
+
+
+
+
+# 5. Print the type of the previous array in question 3
+
+# In[ ]:
+
+
+
+
+
+# 6. Print the numpy version and the configuration
+# 
+
+# In[ ]:
+
+
+
+
+
+# 7. Print the dimension of the array in question 3
+# 
+
+# In[ ]:
+
+
+
+
+
+# 8. Create a boolean array with all the True values
+
+# In[ ]:
+
+
+
+
+
+# 9. Create a two dimensional array
+# 
+# 
+# 
+
+# In[ ]:
+
+
+
+
+
+# 10. Create a three dimensional array
+# 
+# 
+
+# In[ ]:
+
+
+
+
+
+# Difficulty Level **Easy**
+
+# 11. Reverse a vector (first element becomes last)
+
+# In[ ]:
+
+
+
+
+
+# 12. Create a null vector of size 10 but the fifth value which is 1 
+
+# In[ ]:
+
+
+
+
+
+# 13. Create a 3x3 identity matrix
+
+# In[ ]:
+
+
+
+
+
+# 14. arr = np.array([1, 2, 3, 4, 5]) 
+# 
+# ---
+# 
+#  Convert the data type of the given array from int to float 
+
+# In[ ]:
+
+
+
+
+
+# 15. arr1 =          np.array([[1., 2., 3.],
+# 
+#                     [4., 5., 6.]])  
+#                       
+#     arr2 = np.array([[0., 4., 1.],
+#      
+#                    [7., 2., 12.]])
+# 
+# ---
+# 
+# 
+# Multiply arr1 with arr2
+# 
+
+# In[ ]:
+
+
+
+
+
+# 16. arr1 = np.array([[1., 2., 3.],
+#                     [4., 5., 6.]]) 
+#                     
+#     arr2 = np.array([[0., 4., 1.], 
+#                     [7., 2., 12.]])
+# 
+# 
+# ---
+# 
+# Make an array by comparing both the arrays provided above
+
+# In[ ]:
+
+
+
+
+
+# 17. Extract all odd numbers from arr with values(0-9)
+
+# In[ ]:
+
+
+
+
+
+# 18. Replace all odd numbers to -1 from previous array
+
+# In[ ]:
+
+
+
+
+
+# 19. arr = np.arange(10)
+# 
+# 
+# ---
+# 
+# Replace the values of indexes 5,6,7 and 8 to **12**
+
+# In[ ]:
+
+
+
+
+
+# 20. Create a 2d array with 1 on the border and 0 inside
+
+# In[ ]:
+
+
+
+
+
+# Difficulty Level **Medium**
+
+# 21. arr2d = np.array([[1, 2, 3],
+# 
+#                     [4, 5, 6], 
+# 
+#                     [7, 8, 9]])
+# 
+# ---
+# 
+# Replace the value 5 to 12
+
+# In[ ]:
+
+
+
+
+
+# 22. arr3d = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+# 
+# ---
+# Convert all the values of 1st array to 64
+# 
+
+# In[ ]:
+
+
+
+
+
+# 23. Make a 2-Dimensional array with values 0-9 and slice out the first 1st 1-D array from it
+
+# In[ ]:
+
+
+
+
+
+# 24. Make a 2-Dimensional array with values 0-9 and slice out the 2nd value from 2nd 1-D array from it
+
+# In[ ]:
+
+
+
+
+
+# 25. Make a 2-Dimensional array with values 0-9 and slice out the third column but only the first two rows
+
+# In[ ]:
+
+
+
+
+
+# 26. Create a 10x10 array with random values and find the minimum and maximum values
+
+# In[ ]:
+
+
+
+
+
+# 27. a = np.array([1,2,3,2,3,4,3,4,5,6]) b = np.array([7,2,10,2,7,4,9,4,9,8])
+# ---
+# Find the common items between a and b
+# 
+
+# In[ ]:
+
+
+
+
+
+# 28. a = np.array([1,2,3,2,3,4,3,4,5,6])
+# b = np.array([7,2,10,2,7,4,9,4,9,8])
+# 
+# ---
+# Find the positions where elements of a and b match
+# 
+# 
+
+# In[ ]:
+
+
+
+
+
+# 29.  names = np.array(['Bob', 'Joe', 'Will', 'Bob', 'Will', 'Joe', 'Joe'])  data = np.random.randn(7, 4)
+# 
+# ---
+# Find all the values from array **data** where the values from array **names** are not equal to **Will**
+# 
+
+# In[ ]:
+
+
+
+
+
+# 30. names = np.array(['Bob', 'Joe', 'Will', 'Bob', 'Will', 'Joe', 'Joe']) data = np.random.randn(7, 4)
+# 
+# ---
+# Find all the values from array **data** where the values from array **names** are not equal to **Will** and **Joe**
+# 
+# 
+
+# In[ ]:
+
+
+
+
+
+# Difficulty Level **Hard**
+
+# 31. Create a 2D array of shape 5x3 to contain decimal numbers between 1 and 15.
+
+# In[ ]:
+
+
+
+
+
+# 32. Create an array of shape (2, 2, 4) with decimal numbers between 1 to 16.
+
+# In[ ]:
+
+
+
+
+
+# 33. Swap axes of the array you created in Question 32
+
+# In[ ]:
+
+
+
+
+
+# 34. Create an array of size 10, and find the square root of every element in the array, if the values less than 0.5, replace them with 0
+
+# In[ ]:
+
+
+
+
+
+# 35. Create two random arrays of range 12 and make an array with the maximum values between each element of the two arrays
+
+# In[ ]:
+
+
+
+
+
+# 36. names = np.array(['Bob', 'Joe', 'Will', 'Bob', 'Will', 'Joe', 'Joe'])
+# 
+# ---
+# Find the unique names and sort them out!
+# 
+
+# In[ ]:
+
+
+
+
+
+# 37. a = np.array([1,2,3,4,5])
+# b = np.array([5,6,7,8,9])
+# 
+# ---
+# From array a remove all items present in array b
+# 
+# 
+
+# In[ ]:
+
+
+
+
+
+# 38.  Following is the input NumPy array delete column two and insert following new column in its place.
+# 
+# ---
+# sampleArray = numpy.array([[34,43,73],[82,22,12],[53,94,66]]) 
+# 
+# 
+# ---
+# 
+# newColumn = numpy.array([[10,10,10]])
+# 
+
+# In[ ]:
+
+
+
+
+
+# 39. x = np.array([[1., 2., 3.], [4., 5., 6.]]) y = np.array([[6., 23.], [-1, 7], [8, 9]])
+# 
+# 
+# ---
+# Find the dot product of the above two matrix
+# 
+
+# In[ ]:
+
+
+
+
+
+# 40. Generate a matrix of 20 random values and find its cumulative sum
+
+# In[ ]:
+
+
+
+
